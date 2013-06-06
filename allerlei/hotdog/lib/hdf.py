2eac494267f424b060ed59d8acad54738dacabd2
John Evans <john.g.evans.ne@gmail.com>
2013-06-06 16:17:46 -0400

    Added more SDselect, SDendaccess, SDgetinfo

--- allerlei/hotdog/lib/hdf.py
+++ allerlei/hotdog/lib/hdf.py
@@ -7,9 +7,13 @@ ffi.cdef("""
         typedef int int32;
         typedef int intn;
 
+        intn  SDend(int32 id);
+        intn  SDendaccess(int32 sds_id);
+        intn  SDgetinfo(int32 sds_id, char *sds_name, int32 *rank,
+                        int32 dimsizes[], int32 *datatype, int32 *nattrs);
+        intn  SDfileinfo(int32 sd_id, int32 *n_datasets, int32 *n_file_attrs);
+        int32 SDselect(int32 sd_id, int32 sds_index);
         int32 SDstart(char *name, int32 accs);
-        intn SDfileinfo(int32 sd_id, int32 *n_datasets, int32 *n_file_attrs);
-        intn SDend(int32 id);
         """)
 hdf = ffi.verify("""
         #include "mfhdf.h"
@@ -17,6 +21,26 @@ hdf = ffi.verify("""
         libraries=['mfhdf', 'df', 'jpeg', 'z'],
         include_dirs=['/usr/include/hdf'],
         library_dirs=['/usr/lib/hdf'])
+
+def _handle_error(status):
+    if status != 0:
+        raise IOError("Library routine failed.")
+
+def SDselect(sdid, sds_index):
+    sds_id = hdf.SDselect(sdid, sds_index)
+    return sds_id
+
+def SDgetinfo(sds_id):
+    name = ffi.new("char[]", b'\0' * 64)
+    rank = ffi.new("int32 *")
+    status = hdf.SDgetinfo(sds_id, name, rank, ffi.NULL, ffi.NULL, ffi.NULL)
+    _handle_error(status)
+    dimsizes = ffi.new("int32[]", rank[0])
+    datatype = ffi.new("int32 *")
+    nattrs = ffi.new("int32 *")
+    status = hdf.SDgetinfo(sds_id, ffi.NULL, ffi.NULL, dimsizes, datatype, nattrs)
+    _handle_error(status)
+    return ffi.string(name).decode('ascii'), rank, dimsizes, datatype, nattrs
 
 def SDstart(filename, access=DFACC_READ):
     sdid = hdf.SDstart(filename, access)
@@ -26,18 +50,26 @@ def SDfileinfo(sdid):
     nvarsp = ffi.new("int32 *")
     ngattsp = ffi.new("int32 *")
     status = hdf.SDfileinfo(sdid, nvarsp, ngattsp)
-    if status != 0:
-        raise IOError("Library routine failed.")
+    _handle_error(status)
     return nvarsp[0], ngattsp[0]
 
 def SDend(sdid):
     status = hdf.SDend(sdid)
     return status
 
+def SDendaccess(sds_id):
+    status = hdf.SDendaccess(sds_id)
+    _handle_error(status)
+
 if __name__ == "__main__":
 
     file = b'/home/jevans/data/hdf/MODATML2.A2000055.0000.005.2006253045900.hdf'
     sdid = SDstart(file, DFACC_READ) # for read access
     vals = SDfileinfo(sdid)
-    print(vals)
+    for idx in range(vals[0]):
+        sds_id = SDselect(sdid, idx)
+        info = SDgetinfo(sds_id)
+        print(idx)
+        print(info[0])
+        SDendaccess(sds_id)
     SDend(sdid)
