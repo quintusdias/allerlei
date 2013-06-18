904d8c020df0eae57c39318f5504a14f8b50bfce
John Evans <john.g.evans.ne@gmail.com>
2013-06-18 08:27:59 -0400

    Writing baseline tags.

--- HotDog/hotdog/lib/tiff.py
+++ HotDog/hotdog/lib/tiff.py
@@ -5,8 +5,9 @@ from cffi import FFI
 ffi = FFI()
 ffi.cdef("""
         typedef ... TIFF;
-        extern TIFF* TIFFOpen(const char*, const char*);
         void TIFFClose(TIFF *tif);
+        extern TIFF* TIFFOpen(const char*, const char*);
+        extern int TIFFSetField(TIFF*, uint32_t, ...);
         """)
 _lib = ffi.verify("""
         #include "tiffio.h"
@@ -14,6 +15,23 @@ _lib = ffi.verify("""
         libraries=['tiff', 'jpeg', 'z'],
         include_dirs=['/usr/include/hdf', '/opt/local/include'],
         library_dirs=['/opt/local/lib'])
+
+tags_int16 = ['PhotometricInterpretation', 'PlanarConfiguration']
+tags_int32 = ['BitsPerSample', 'ImageWidth', 'ImageLength', 'SamplesPerPixel',
+              'TileWidth', 'TileLength']
+tagnumber = {'ImageWidth': 256,
+             'ImageLength': 257,
+             'BitsPerSample': 258,
+             'PhotometricInterpretation': 262,
+             'SamplesPerPixel': 277,
+             'PlanarConfiguration': 284,
+             'TileWidth': 322,
+             'TileLength': 323}
+
+PLANARCONFIG_CONTIG = 1
+PLANARCONFIG_SEPARATE = 2
+
+PHOTOMETRIC_RGB = 2
 
 def _handle_error(status):
     if status < 0:
@@ -24,6 +42,18 @@ def open(filename, mode='r'):
     tiffp = _lib.TIFFOpen(filename.encode(), mode.encode())
     yield tiffp
     _lib.TIFFClose(tiffp)
+
+def setfield(tifp, tagname, *args):
+    if tagname in tags_int16:
+        value = ffi.cast("short", args[0])
+        status = _lib.TIFFSetField(tifp, tagnumber[tagname], value);
+    elif tagname in tags_int32:
+        value = ffi.cast("int", args[0])
+        status = _lib.TIFFSetField(tifp, tagnumber[tagname], value);
+    else:
+        raise NotImplementedError("Unrecognized tag.")
+
+    _handle_error(status)
 
 def close(tiffp):
     _lib.TIFFclose(tiffp)
--- HotDog/hotdog/test/test_tiff.py
+++ HotDog/hotdog/test/test_tiff.py
@@ -19,7 +19,16 @@ class TestTiff(unittest.TestCase):
     def test_basic(self):
         with tempfile.NamedTemporaryFile(suffix=".tif") as tfile:
             with TIFF.open(tfile.name, 'w') as tifp:
-                print(tifp)
+                TIFF.setfield(tifp, 'ImageWidth', 1024)
+                TIFF.setfield(tifp, 'ImageLength', 1024)
+                TIFF.setfield(tifp, 'SamplesPerPixel', 3)
+                TIFF.setfield(tifp, 'BitsPerSample', 8)
+                TIFF.setfield(tifp, 'PlanarConfiguration', TIFF.PLANARCONFIG_CONTIG)
+                TIFF.setfield(tifp, 'PhotometricInterpretation', TIFF.PHOTOMETRIC_RGB)
+                TIFF.setfield(tifp, 'TileWidth', 256)
+                TIFF.setfield(tifp, 'TileLength', 256)
+            import shutil
+            shutil.copyfile(tfile.name, '/home/jevans/a.tif')
 
 if __name__ == "__main__":
     unittest.main()
