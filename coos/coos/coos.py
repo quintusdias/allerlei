a18b3c1bea04a4346f76b7f0ca71b07ba5601909
jevans <john.g.evans.ne@gmail.com>
2013-08-01 20:33:29 -0400

    parses ascii radials into sections

--- coos/coos/coos.py
+++ coos/coos/coos.py
@@ -1,11 +1,41 @@
+import itertools
+
 class CodarRadials(object):
     """CODAR radials.
     """
     def __init__(self):
         pass
 
-    def parse_ascii(self, ascii_file):
+    def parse(self, ascii_file):
+        """Parse ASCII radials file.
+
+        Parameters
+        ----------
+        ascii_file : str
+            Path to ASCII radials file.
+        """
+        with open(ascii_file, 'r', encoding='latin1') as fptr:
+            # Iterate thru the file until hitting the end of the table section.
+            gen = itertools.takewhile(lambda x: not x.startswith('%TableEnd:'),
+                                      fptr)
+            lluv = list(gen)
+
+            gen = itertools.takewhile(lambda x: not x.startswith('%TableEnd:'),
+                                      fptr)
+            rads1 = list(gen)
+
+            gen = itertools.takewhile(lambda x: not x.startswith('%TableEnd:'),
+                                      fptr)
+            rcv2 = line(gen)
+            footer = fptr.readlines()
+
         pass
+
+    def parse_header(self, fptr):
+        """
+        """
+        while True:
+            line = fptr.readline()
 
     def write_netcdf(self, netcdf_file):
         pass
