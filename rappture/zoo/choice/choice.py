aea84afa4281a073683c51f1c706ed04726654c1
jevans <john.g.evans.ne@gmail.com>
2013-09-05 15:16:16 -0400

    No need for driver, just use "io" instead.

--- rappture/zoo/choice/choice.py
+++ rappture/zoo/choice/choice.py
@@ -6,12 +6,11 @@ import sys
 import Rappture
 
 # open the XML file containing the run parameters
-driver = Rappture.library('tool.xml')
 io = Rappture.library(sys.argv[1])
 
-driver.put("output.choice(outs).about.label", "Echo of choice")
+io.put("output.choice(outs).about.label", "Echo of choice")
 choice = io.get("input.(stats).current")
-driver.put("output.choice(outs).current", choice)
+io.put("output.choice(outs).current", choice)
 
-Rappture.result(driver)
+Rappture.result(io)
 sys.exit()
