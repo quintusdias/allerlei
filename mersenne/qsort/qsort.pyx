a7176f82b4d4c0a8da12f03d5e64357a93608e83
jevans <john.g.evans.ne@gmail.com>
2014-10-26 22:16:14 -0400

    up to safe_qsort

--- mersenne/qsort/qsort.pyx
+++ mersenne/qsort/qsort.pyx
@@ -4,7 +4,11 @@ cdef extern from "stdlib.h":
     void *malloc(size_t size)
     void free(void *ptr)
 
-def pyqsort(list x):
+ctypedef int (*qsort_cmp)(const void *, const void *)
+
+def pyqsort(list x, cmp=None, reverse=False):
+    global py_cmp
+
     cdef:
         int *array
         int i, N
@@ -19,8 +23,21 @@ def pyqsort(list x):
     for i in range(N):
         array[i] = x[i]
 
+    cdef qsort_cmp cmp_callback
+
+    if cmp and reverse:
+        py_cmp = cmp
+        cmp_callback = reverse_py_cmp_wrapper
+    elif cmp and not reverse:
+        py_cmp = cmp
+        cmp_callback = py_cmp_wrapper
+    elif reverse:
+        cmp_callback = reverse_int_compare
+    else:
+        cmp_callback = int_compare
+
     # qsort the array...
-    qsort(<void*>array, <size_t>N, sizeof(int), int_compare)
+    result = safe_qsort(<void*>array, <size_t>N, sizeof(int), cmp_callback)
 
     # Convert back into Python and free the C array.
     for i in range(N):
@@ -32,3 +49,22 @@ cdef int int_compare(const void *a, const void *b):
     ia = (<int*>a)[0]
     ib = (<int*>b)[0]
     return ia - ib
+
+cdef int reverse_int_compare(const void *a, const void *b):
+    return -int_compare(a, b)
+
+cdef object py_cmp = None
+
+cdef int py_cmp_wrapper(const void *a, const void *b):
+    cdef int ia, ib
+    ia = (<int*>a)[0]
+    ib = (<int*>b)[0]
+    try:
+        result = py_cmp(ia, ib)
+    except:
+        result = -2
+    return result
+
+cdef int reverse_py_cmp_wrapper(const void *a, const void *b):
+    return -py_cmp_wrapper(a, b)
+
