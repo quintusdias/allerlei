d319936b816adec484bd8e1c37946171d638dfb5
jevans <john.g.evans.ne@gmail.com>
2014-12-08 22:03:26 -0500

    magnitude from planet and size from planet should be in the planet table

--- kepler/extract_stars.sh
+++ kepler/extract_stars.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /opt/data/csv/phl_hec_all_kepler.csv | tail -n +2  | awk -F, -v OFS=',' '{print $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49, $50, $51, $52, $53, $54}'  | sort --field-separator=, -u -k1,1 
+cat /opt/data/csv/phl_hec_all_kepler.csv | tail -n +2  | awk -F, -v OFS=',' '{print $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $51, $52, $53, $54}'  | sort --field-separator=, -u -k1,1 
