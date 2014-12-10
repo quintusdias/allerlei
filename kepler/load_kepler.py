e6537b12a7fad78c4934eca33fdb4f50272b0df3
jevans <john.g.evans.ne@gmail.com>
2014-12-09 20:28:00 -0500

    stuff

--- kepler/load_kepler.py
+++ kepler/load_kepler.py
@@ -2,9 +2,13 @@ import pandas as pd
 import psycopg2
 import sqlalchemy
 
-connect_string = 'postgresql+psycopg2://jevans@localhost/kepler'
+connect_string = 'postgresql+psycopg2://jevans:99luftbaloons@localhost:5432/kepler'
 engine = sqlalchemy.create_engine(connect_string)
 
 conn = psycopg2.connect(database='kepler')
+cursor = conn.cursor()
 df = pd.io.sql.read_sql('select * from star', engine)
-print(df)
+columns = df.columns[1:]
+stars_df = pd.read_csv('stars.dat', header=None, index_col=None)
+stars_df.columns = columns
+stars_df.to_sql('star', engine, if_exists='append', index=False)
