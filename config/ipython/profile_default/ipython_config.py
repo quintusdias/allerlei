5dff14686c37781a071db4658c90ed97b45a49c0
John Evans <john.g.evans.ne@gmail.com>
2017-09-02 12:49:40 -0400

    Add editor, exec_lines

--- config/ipython/profile_default/ipython_config.py
+++ config/ipython/profile_default/ipython_config.py
@@ -1,1 +1,14 @@
+# Shortcut style to use at the prompt.  'vi' or 'emacs'
 c.TerminalInteractiveShell.editing_mode = 'vi'
+
+# Set the editor used by IPython
+c.TerminalInteractiveShell.editor = 'vim'
+
+# Lines of code to run at IPython startup
+c.InteractiveShellApp.exec_lines = [
+    'import matplotlib.pyplot as plt',
+    'import numpy as np',
+    'import pandas as pd',
+    'import seaborn as sns',
+]
+
